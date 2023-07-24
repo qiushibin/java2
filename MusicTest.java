0539745a8f25f2204db3191deee6d2edde6f6cd4
qiushibin <qiu.shibin@cxtc.com>
2023-07-24 16:30:32 +0800

    modified:   MusicTest.java 	modified:   .DS_Store 	modified:   .vscode/settings.json

--- MusicTest.java
+++ MusicTest.java
@@ -1,19 +1,52 @@
 import javax.sound.midi.*;
 
-
-public class MusicTest{
-    public void play(){
+public class MusicTest {
+    public void play() throws badex{
         try{
             Sequencer  sequencer = MidiSystem.getSequencer();
+            
+            int i = 11;
+            if(i>10){
+                throw new badex();
+            }
+            
+            sequencer.open();
+            Sequence seq = new Sequence(Sequence.PPQ,4);
+            Track t = seq.createTrack();
+            
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            t.add(noteOn);
+            
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            t.add(noteOff);
+
+            sequencer.setSequence(seq);
+            sequencer.start();
             System.out.println("Successfully got a sequencer");
-        } catch(MidiUnavailableException ex){
+
+        } catch(MidiUnavailableException | InvalidMidiDataException ex){
             System.out.println("Bummer");
+            ex.printStackTrace();
         }
+        
     }
 
-    public static void main(string[] args){
+    public class badex extends Exception{
+    }
+
+    public static void main(String[] args){
         MusicTest mt = new MusicTest();
-        mt.play();
+        try{
+            mt.play();
+        }catch(badex ex){
+            System.out.println("badex");
+        }finally{
+            System.out.println("finally");
+        }
     }
 
 }
