bbbfe9cb39bc3dd1f3fc375b38d76059bf20cfc5
qiushibin <qiu.shibin@cxtc.com>
2023-07-25 16:38:42 +0800

    modified:   GUITest.java 	new file:   bin/GUITest$inclass.class 	modified:   bin/GUITest.class 	new file:   bin/drawpanel.class 	new file:   drawpanel.java

--- GUITest.java
+++ GUITest.java
@@ -1,15 +1,53 @@
 import javax.swing.*;
 
-public class GUITest{
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.*;
+
+public class GUITest {
+    JButton button ;
+    JRadioButton  rad ;
+    JFrame frame;
     public static void main (String[] args){
-        JFrame frame = new JFrame();
-        JButton button = new JButton("chick me", null);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(button);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-
-
+        GUITest gui = new GUITest();
+        gui.play();
     }
+
+    public void play(){
+     frame = new JFrame();
+       
+        rad = new JRadioButton("choose");
+        button = new JButton("chick me", null);
+        button.addActionListener(new inclass());
+
+        
+        JPanel jpb =  new JPanel();
+        jpb.setLayout(new BorderLayout());
+        jpb.setSize(50, 50);
+        jpb.setBackground(Color.white);
+        
+        jpb.add(rad,BorderLayout.WEST);
+        jpb.add(button,BorderLayout.EAST);
+        
+        drawpanel dp = new drawpanel();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.SOUTH ,jpb);
+        frame.getContentPane().add(BorderLayout.CENTER ,dp);
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+        frame.setTitle("ddGUIdd");
+        
+    }
+
+   class inclass implements ActionListener{
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == button)
+            button.setText("2222");
+       frame.repaint();
+    }
+   }
+
+    
 }
---
+++ drawpanel.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.*;
+
+class drawpanel extends JPanel{
+    
+    public void paintComponent(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+
+        int red = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        Color   sc = new Color(red, blue, green);
+
+         red = (int) (Math.random() * 255);
+         blue = (int) (Math.random() * 255);
+         green = (int) (Math.random() * 255);
+        Color   ec = new Color(red, blue, green);
+
+        GradientPaint grad = new GradientPaint(70, 70, sc, 200, 200, ec);
+        g2d.setPaint(grad);
+        g2d.fillOval(200, 70, 100, 100);
+    }
+
+}
