0f0c4c8ecb7e437cb725ed27f68c6d83fea7ccaf
qiushibin <qiu.shibin@cxtc.com>
2023-07-25 17:32:40 +0800

    modified:   GUITest.java 	modified:   bin/GUITest$inclass.class 	modified:   bin/GUITest.class

--- GUITest.java
+++ GUITest.java
@@ -19,20 +19,26 @@ public class GUITest {
         rad = new JRadioButton("choose");
         button = new JButton("chick me", null);
         button.addActionListener(new inclass());
-
+        JLabel lab = new JLabel("label");
+        lab.setForeground(Color.GREEN);
         
         JPanel jpb =  new JPanel();
-        jpb.setLayout(new BorderLayout());
-        jpb.setSize(50, 50);
-        jpb.setBackground(Color.white);
+        //jpb.setLayout(new BorderLayout());
+        jpb.setBackground(Color.DARK_GRAY);
+        //jpb.add(rad);
+        jpb.add(lab);
         
-        jpb.add(rad,BorderLayout.WEST);
-        jpb.add(button,BorderLayout.EAST);
-        
+        JPanel jpb2 =  new JPanel();
+        jpb2.setLayout(new BoxLayout(jpb2,BoxLayout.Y_AXIS));
+        jpb2.setBackground(Color.LIGHT_GRAY);
+        jpb2.add(rad);
+        jpb2.add(button);
+
         drawpanel dp = new drawpanel();
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(BorderLayout.SOUTH ,jpb);
+        frame.getContentPane().add(BorderLayout.EAST ,jpb2);
         frame.getContentPane().add(BorderLayout.CENTER ,dp);
         frame.setSize(500, 300);
         frame.setVisible(true);
@@ -41,12 +47,14 @@ public class GUITest {
     }
 
    class inclass implements ActionListener{
+
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         if(e.getSource() == button)
             button.setText("2222");
        frame.repaint();
     }
+    
    }
 
     
